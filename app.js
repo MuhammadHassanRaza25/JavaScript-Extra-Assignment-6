82d543531139b6983155981cf98f5f5c578ac62c
unknown <Muhammad Hassan Raza>
2024-05-08 03:08:45 +0500

    JavaScrippt Extra Assignment 6

---
+++ app.js
@@ -0,0 +1,17 @@
+// JavaScript Extra Assignment 6 //
+
+// Guess Number Between 1 and 10
+
+function guess(){
+var userVal = prompt('Enter Guess Number Between 1 and 10')
+var guessNum = 8
+if(userVal == guessNum){
+    alert('Good Work 👍🏻')
+}
+else{
+    alert('Not Matched')
+}
+}
+
+
+
